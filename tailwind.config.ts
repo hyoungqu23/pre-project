179e19b3b69c03d525b8baba9f665bae90fba88a
HyoungMin <hyoungqu23@gmail.com>
2024-10-17 16:41:30 +0900

    Feature Sliced Design 아키텍처에 적합하게 Tailwind CSS의 configuration을 업데이트한다

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/app/**/*.{ts,tsx,mdx}',
+    './src/widgets/**/*.{ts,tsx,mdx}',
+    './src/entities/**/*.{ts,tsx,mdx}',
+    './src/shared/**/*.{ts,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
