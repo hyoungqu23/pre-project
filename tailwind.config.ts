7079070d6242f0080398cf41e4e5b9a06adb13fe
HyoungMin <hyoungqu23@gmail.com>
2024-10-31 16:15:06 +0900

    Typography 커스텀 플러그인을 생성하고, 이를 적용한다

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,57 @@
 import tailwindcssAspectRatioPlugin from '@tailwindcss/aspect-ratio';
 import tailwindcssFormsPlugin from '@tailwindcss/forms';
 import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
+
+const generateValues = (fontWeights: string[]) => {
+  const fontSizes = [
+    8, 10, 12, 14, 16, 18, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 72,
+    80, 96,
+  ];
+
+  return Object.fromEntries(
+    fontSizes.flatMap((fontSize) =>
+      fontWeights.map((fontWeight) => [
+        `${fontSize}-${fontWeight}`,
+        `${fontSize}-${fontWeight}`,
+      ]),
+    ),
+  );
+};
+
+const typographyPlugin = plugin(({ matchUtilities, theme }) => {
+  const createTypographyStyles = (value: string) => {
+    const [fontSizeStr, fontWeightStr] = value.split('-').map((v) => v.trim());
+    const fontSize = parseInt(fontSizeStr, 10);
+    const fontWeight = theme(`fontWeight.${fontWeightStr}`) || 'normal';
+
+    const viewportFontSize = (fontSize / 1920) * 100;
+    const responsiveFontSize = `min(${fontSize}px, ${viewportFontSize}vw)`;
+
+    return {
+      fontSize: responsiveFontSize,
+      lineHeight: theme('lineHeight.normal') || '150%',
+      fontWeight,
+    };
+  };
+
+  matchUtilities(
+    {
+      'typo-display': createTypographyStyles,
+      'typo-title': createTypographyStyles,
+      'typo-body': createTypographyStyles,
+      'typo-caption': createTypographyStyles,
+    },
+    {
+      values: {
+        ...generateValues(['bold', 'extrabold']),
+        ...generateValues(['medium', 'semibold', 'bold']),
+        ...generateValues(['normal', 'medium', 'semibold', 'bold']),
+        ...generateValues(['light', 'normal', 'medium']),
+      },
+    },
+  );
+});
 
 const config: Config = {
   content: [
@@ -15,6 +66,7 @@ const config: Config = {
   plugins: [
     tailwindcssFormsPlugin,
     tailwindcssAspectRatioPlugin,
+    typographyPlugin,
   ],
 };
 
